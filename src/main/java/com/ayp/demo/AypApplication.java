07bc528a73292dbb843bb6e54905ef60e2b61682
774697399@qq.com <ass6120524481314>
2018-05-06 12:33:50 +0800

    first commit

---
+++ src/main/java/com/ayp/demo/AypApplication.java
@@ -0,0 +1,12 @@
+package com.ayp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AypApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AypApplication.class, args);
+	}
+}
