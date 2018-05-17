9f44544bb2451ec3ca39b9f43d208e6b64122131
qiumeng <774697399@qq.com>
2018-05-17 13:55:23 +0800

    配置mybatis

--- src/main/java/com/ayp/AypApplication.java
+++ src/main/java/com/ayp/AypApplication.java
@@ -1,9 +1,13 @@
-package com.ayp.demo;
+package com.ayp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan("com.ayp.web.dao")
+@ComponentScan("com.ayp")
 public class AypApplication {
 
 	public static void main(String[] args) {
