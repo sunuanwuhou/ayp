a70aa7ea7fc233719c36c9dc920d97d2cb917051
qiumeng <774697399@qq.com>
2018-05-17 14:33:36 +0800

    配置mybatis

--- src/main/java/com/ayp/AypApplication.java
+++ src/main/java/com/ayp/AypApplication.java
@@ -3,14 +3,20 @@ package com.ayp;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.ayp.web.dao")
 @ComponentScan("com.ayp")
-public class AypApplication {
+public class AypApplication  extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
+    public static void main(String[] args) {
 		SpringApplication.run(AypApplication.class, args);
 	}
 }
---
+++ src/main/java/com/ayp/web/controller/AypController.java
@@ -0,0 +1,27 @@
+package com.ayp.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author qiumeng
+ * @Package com.ayp.web.controller
+ * @Description:
+ * @date 2018 2018/5/17 13:58
+ */
+@Controller
+public class AypController {
+
+
+    /**
+     * 测试hello
+     * @return
+     */
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String hello(Model model) {
+        model.addAttribute("name", "Dear");
+        return "hello";
+    }
+}
